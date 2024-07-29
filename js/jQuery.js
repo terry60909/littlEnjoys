61c86b1e26dbb372f9ce9a452836c8cac383911b
terry60909 <terry60909@yahoo.com.tw>
2024-07-29 08:45:50 +0800

    加上部分js

---
+++ js/jQuery.js
@@ -0,0 +1,60 @@
+$(function () {
+    let divWidth = $('#main').width();
+    let imgCount = $('#content li').length;
+ 
+    for(let i = 0; i < imgCount; i++){
+     $('#contentButton').append(`<li></li>`)
+     }
+     $('#contentButton li:first').addClass('clicked')
+ 
+     $('#content li').width(divWidth)
+     
+     $('#content').width(divWidth * imgCount)
+         
+     let index = 0;
+     let timer = setInterval(moveToNext, 5000)
+ 
+     $('#contentButton li').click(function(){
+        clearInterval(timer)
+ 
+         index = $(this).index()
+ 
+        $('#content').animate({
+         left: divWidth * index * -1
+        })
+ 
+        $(this).addClass('clicked')
+        $('#contentButton li').not(this).removeClass('clicked')
+ 
+        timer = setInterval(moveToNext, 5000)
+     })
+ 
+     function moveToNext(){
+         if(index < imgCount - 1){
+             index += 1
+         }else{
+             index = 0
+         }
+         
+         $('#content').animate({
+             left: divWidth * index * -1
+            })
+ 
+            $(`#contentButton li:eq(${index})`).addClass('clicked')
+            $('#contentButton li').not(`:eq(${index})`).removeClass('clicked')
+     }
+ 
+ });
+ 
+ 
+ 
+ 
+ 
+      $(document).ready(function(){
+             $('figure').click(function(){
+                 let picSrc = $(this).find('img').attr('src')
+                 $('#pic').attr('src',picSrc)
+                 $(this).addClass('active')
+                 $('figure').not(this).removeClass('active')
+             })
+         })
